8353b2fc0a5a448c7a9bdfc282eb8168162098d2
varshitha-sahukari <svarshitha49@gmail.com>
2026-07-14 11:52:54 +0530

    Time: 0 ms (100%), Space: 42.7 MB (56.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,11 @@
 class Solution {
-    int solve(int i, int[] nums, int[] dp){
-        if(i>=nums.length) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int take = nums[i] + solve(i+2,nums,dp);
-        int dontTake = solve(i+1,nums,dp);
-        return dp[i] = Math.max(take,dontTake);
-    }
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return solve(0,nums,dp);
+        int [] dp = new int[nums.length+2];
+        for(int i=nums.length-1;i>=0;i--){
+            int take = nums[i] + dp[i+2];
+            int dontTake = dp[i+1];
+            dp[i] = Math.max(take,dontTake);
+        }
+        return dp[0];
     }
 }
