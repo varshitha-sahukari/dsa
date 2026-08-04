940bdb5355d3c0a9d06df8e37585c1466863376a
varshitha-sahukari <svarshitha49@gmail.com>
2026-08-04 11:15:43 +0530

    Time: 2 ms (89.92%), Space: 44.4 MB (24.93%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,39 +1,35 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        
-        int m = grid.length;
-        int n = grid[0].length;
+        int n = grid.length;
+        int m = grid[0].length;
         int fresh = 0;
         Queue<int[]> q = new LinkedList<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.offer(new int[]{i,j});
-                }
-                else if(grid[i][j]==1){
+                }else if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
-        int [] dr = {-1,0,1,0};
-        int [] dc = {0,1,0,-1};
-        int time = 0;
         if(fresh == 0){
             return 0;
         }
-
+        int []dr = {-1,0,1,0};
+        int []dc = {0,1,0,-1};
+        int time = 0;
         while(!q.isEmpty()){
             int size = q.size();
-
-            while(size-- > 0){
-                int[]curr = q.poll();
-                int r = curr[0];
-                int c = curr[1];
+            while(size-- >0){
+                int[] temp = q.poll();
+                int r = temp[0];
+                int c = temp[1];
                 for(int k=0;k<4;k++){
-                    int nr = r+dr[k];
+                    int nr = r +dr[k];
                     int nc = c +dc[k];
-                    if(nr >=0 && nr <m && nc>=0 && nc <n){
-                        if(grid[nr][nc] ==1){
+                    if(nr >=0 && nr<n && nc>=0 && nc<m){
+                        if(grid[nr][nc]== 1){
                             grid[nr][nc] =2;
                             fresh--;
                             q.offer(new int[]{nr,nc});
@@ -44,7 +40,6 @@ class Solution {
             }
             time++;
         }
-
-    return fresh == 0 ? time - 1 : -1;
+        return fresh == 0 ? time - 1 : -1;
     }
 }
