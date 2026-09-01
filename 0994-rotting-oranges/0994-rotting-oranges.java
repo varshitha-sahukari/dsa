05f521629e86bf867ab6a238c193bdf946ffcc95
varshitha-sahukari <svarshitha49@gmail.com>
2026-09-01 14:47:10 +0530

    Time: 2 ms (89.32%), Space: 44.2 MB (56.19%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,45 +1,40 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int fresh = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int total = 0;
+        int rotten =0;
         Queue<int[]> q = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]!=0) total++;
                 if(grid[i][j]==2){
+                    rotten++;
                     q.offer(new int[]{i,j});
-                }else if(grid[i][j]==1){
-                    fresh++;
                 }
             }
         }
-        if(fresh == 0){
-            return 0;
-        }
-        int []dr = {-1,0,1,0};
-        int []dc = {0,1,0,-1};
-        int time = 0;
-        while(!q.isEmpty()){
+        int []dr = {-1,1,0,0};
+        int []dc = {0,0,-1,1};
+        int time =0;
+        while(!q.isEmpty() && rotten < total){
             int size = q.size();
-            while(size-- >0){
-                int[] temp = q.poll();
-                int r = temp[0];
-                int c = temp[1];
+            for(int i=0;i<size;i++){
+                int[] cell = q.poll();
+                int r = cell[0];
+                int c = cell[1];
                 for(int k=0;k<4;k++){
-                    int nr = r +dr[k];
-                    int nc = c +dc[k];
-                    if(nr >=0 && nr<n && nc>=0 && nc<m){
-                        if(grid[nr][nc]== 1){
-                            grid[nr][nc] =2;
-                            fresh--;
-                            q.offer(new int[]{nr,nc});
-                        }
+                    int nr = r + dr[k];
+                    int nc = c + dc[k];
+                    if(nr >=0 && nr < rows && nc >=0 && nc < cols && grid[nr][nc] == 1){
+                        rotten++;
+                        grid[nr][nc] = 2;
+                        q.offer(new int[]{nr,nc});
                     }
                 }
-                
             }
             time++;
         }
-        return fresh == 0 ? time - 1 : -1;
+        return rotten==total ? time : -1;
     }
 }
