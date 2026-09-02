fd04caf38860b4107b69666d882303c39d34f1b6
varshitha-sahukari <svarshitha49@gmail.com>
2026-09-02 16:09:32 +0530

    Time: 21 ms (54.08%), Space: 48.3 MB (36.78%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,49 +1,37 @@
-import java.util.*;
-
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-
         int[] freq = new int[26];
-        for (char task : tasks) {
-            freq[task - 'A']++;
+        for(char task : tasks){
+            freq[task-'A']++;
         }
-        PriorityQueue<Integer> pq =
-            new PriorityQueue<>((a, b) -> b - a);
-        for (int f : freq) {
-            if (f > 0) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        for(int f:freq){
+            if(f>0){
                 pq.add(f);
             }
         }
-
-        int time = 0;
-
-        while (!pq.isEmpty()) {
-            int cycle = n + 1;
-            List<Integer> remaining = new ArrayList<>();
-
-            while (cycle > 0 && !pq.isEmpty()) {
-
+        int time =0;
+        
+        while(!pq.isEmpty()){
+            ArrayList<Integer> remaining = new ArrayList<>();
+            int cycle = n+1;
+            while(!pq.isEmpty() && cycle>0){
                 int current = pq.poll();
-
                 current--;
-
-                if (current > 0) {
+                time++;
+                cycle--;
+                if(current>0){
                     remaining.add(current);
                 }
 
-                time++;
-                cycle--;
             }
-
-            for (int f : remaining) {
+            for(int f : remaining){
                 pq.add(f);
             }
-
-            if (!pq.isEmpty()) {
+            if(!pq.isEmpty()){
                 time += cycle;
             }
         }
-
         return time;
     }
 }
